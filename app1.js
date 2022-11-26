39867d47fea82b0dc69f96f63ececdcde207d29f
Leonardo Slepack <111914236+LeoSlepack@users.noreply.github.com>
2022-11-25 21:09:29 -0300

    Initial commit

---
+++ app1.js
@@ -0,0 +1,21 @@
+let numero1 = 10;
+let numero2 = 10;
+let resultado = numero1 + numero2;
+
+
+if (resultado > 29) {
+
+    console.log("é maior que 29");
+
+} else if (resultado == 29) {
+
+    console.log("o resultado é idêntico a 29");
+
+} else {
+
+    console.log("o resultado é menor que 29");
+
+}
+
+console.log("o resultado é: ");
+console.log(resultado);
